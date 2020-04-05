0fe6150701f3a9510a38326d87c9496200b96013
bohdanlevkovych <fnx@ukr.net>
2020-04-05 12:56:57 +0300

    test

--- src/main/java/control/center/bot/mappers/WebmToMp4Service.java
+++ src/main/java/control/center/bot/mappers/WebmToMp4Service.java
@@ -4,17 +4,62 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class WebmToMp4Service {
 
     public static String mapToMp4(String webm) {
         try {
+            validateSize(webm);
             File file = downloadFromUrl(webm);
             return (convertWebmToMp4(file.getPath()));
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return null;
+        }
+    }
+
+    public static Double getFileSizeByLink(String link) {
+        if (!link.contains("https://")) {
+            link = "https://" + link;
+        }
+        URLConnection conn = null;
+        URL url = null;
+        try {
+            url = new URL(link);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            conn = url.openConnection();
+            if (conn instanceof HttpURLConnection) {
+                ((HttpURLConnection) conn).setRequestMethod("HEAD");
+            }
+            conn.getInputStream();
+            int size = conn.getContentLength();
+            return BigDecimal.valueOf(size)
+                    .divide(BigDecimal.valueOf(1048576))
+                    .setScale(2, 2)
+                    .doubleValue();
+        } catch (IOException e) {
+            System.out.println("error on " + url);
+            e.printStackTrace();
+            return 3.0;
+        } finally {
+            if (conn instanceof HttpURLConnection) {
+                ((HttpURLConnection) conn).disconnect();
+            }
+        }
+    }
+
+    public static void validateSize(String link) {
+        Double fileSizeByLink = getFileSizeByLink(link);
+        if (fileSizeByLink > 20) {
+            throw new RuntimeException();
         }
     }
 
